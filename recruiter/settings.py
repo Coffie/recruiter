dd653c9a6af00a9ef6d4bf2b1fc1c003c01d6892
Christoffer Nysæther <christoffer.nysaether@gmail.com>
2017-07-13 04:35:23 -0400

    Add media folders to settings

--- recruiter/settings.py
+++ recruiter/settings.py
@@ -130,6 +130,8 @@ STATIC_URL = '/static/'
 #STATICFILES_DIRS = ""
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 EMAIL_HOST = 'localhost'
 EMAIL_PORT = 8000
